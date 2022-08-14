224f688ced61c127e39db9b4a36519394358be1d
jayBpop <captainsparrow560@gmail.com>
2022-08-14 17:36:43 +0530

    remove the commit messages from the pushDockerImage file

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,17 +1,10 @@
 def call(String imageName){
 
     echo "Pushing the docker image to docker hub!"
-    
-    withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable:'password', usernameVariable:'username')])
 
-<<<<<<< HEAD
-   {
-        sh 'echo $password | docker login -u $username --password-stdin'
-        sh 'docker push hdevop/myrepo:jsl_java_app-1.0'
-=======
     withCredentials([(usernamePassword(credentialsId:'docker-hub-repo', passwordVariable:'password', usernameVariable:'username')]){
         sh 'echo $password | docker login -u $username --password-stdin'
         sh "docker push $imageName"
->>>>>>> 0454e11 (passing imageName as parameter)
+
     }
 }
