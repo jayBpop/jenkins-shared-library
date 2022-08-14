71ef1cf46c5f8dd0e07e837da9adc15610f8c566
jayBpop <86476698+jayBpop@users.noreply.github.com>
2022-08-14 16:21:42 +0530

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -2,7 +2,7 @@ def call(){
 
     echo "Pushing the docker image to docker hub!"
 
-    withCredentials[(usernamePassword:(credentialsId:'docker-hub-repo', passwordVariable:'password', usernameVariable:'username'))]{
+    withCredentials([(usernamePassword(credentialsId:'docker-hub-repo', passwordVariable:'password', usernameVariable:'username')]){
         sh 'echo $password | docker login -u $username --password-stdin'
         sh 'docker push hdevop/myrepo: jsl_java_app-1.0'
     }
